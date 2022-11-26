326e85227e25baed42df3efb0ee5af9884775ec9
alaxhenry <alaxhenry0121@gmail.com>
2022-11-26 13:03:13 +0900

    Initial Commit

---
+++ ChatAppUIMacOS/ChatAppUIMacOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatAppUIMacOSApp.swift
+//  ChatAppUIMacOS
+//
+//  Created by Seungchul Ha on 2022/11/26.
+//
+
+import SwiftUI
+
+@main
+struct ChatAppUIMacOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
